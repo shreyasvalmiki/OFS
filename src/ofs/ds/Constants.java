69dc15290b144230d59d1085bd09bdf4da0da844
svalmiki <shreyasvalmiki@gmail.com>
2012-11-18 21:43:50 -0500

    Bitmap Update

--- src/ofs/ds/Constants.java
+++ src/ofs/ds/Constants.java
@@ -2,4 +2,8 @@ package ofs.ds;
 
 public class Constants {
 	public static final int SUPERBLOCK_SIZE = 60;
+	public static final int WORD_SIZE = 32;
+	public static final int WORD_SHIFT = 5;
+	public static final int HEX_WORD_SIZE = 0x1F;
+	public static final int HEX_EMPTY_WORD = 0x80000000;
 }
