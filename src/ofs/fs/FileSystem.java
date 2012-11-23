bbfb94596c1681f8354eaa39e67628c60f105441
svalmiki <shreyasvalmiki@gmail.com>
2012-11-22 22:42:25 -0500

    File create

--- src/ofs/fs/FileSystem.java
+++ src/ofs/fs/FileSystem.java
@@ -3,43 +3,60 @@ package ofs.fs;
 import java.io.*;
 import java.util.Scanner;
 
-import ofs.ds.*;
+//import ofs.ds.*;
 
 public class FileSystem {
 	
-	String sep;
+	private static String sep;
+	private String fileName;
+	private int fileSize;
+	private int blockSize;
+	
 	public FileSystem(){
-		String sep = File.separator;
+		sep = File.separator;
+		System.out.println(sep);
 	}
-	public static void main(){
+	public static void main(String[] args){
+		boolean fileNameOk = false;
 		FileSystem fs = new FileSystem();
 		Scanner in = new Scanner(System.in);
-		String fileName = new String();
-		System.out.println("Enter the file system you want to use (example: file.dat):");
-		fileName = in.next();
-		if (!fileName.substring(-4,-1).equals(".dat"))
-		{
-			System.out.println("Invalid file extension");
-		}
-		else
-		{
-			if(!fs.isCreated(fileName))
+		//String fileName = new String();
+		
+		do{
+			System.out.println("Enter the file system file you want to use (example: file.dat):");
+			fs.fileName = in.next();
+			if (!fs.fileName.substring(fs.fileName.length()-4).equals(".dat"))
 			{
-				System.out.println(fileName + " is not avalable. Do you want to create? [Y/N]");
-				if(in.next().toUpperCase() == "Y"){
-					System.out.println("Enter file size in order of 2 (eg. 256):");
-					int fileSize = in.nextInt();
-					System.out.println("Enter file size in order of 2 (eg. 128):");
-					int blockSize = in.nextInt();
-					fs.create(fileName, fileSize, blockSize);
+				System.out.println("Invalid file extension");
+				fileNameOk = false;
+			}
+			else
+			{
+				if(!fs.isCreated(fs.fileName))
+				{
+					System.out.println(fs.fileName + " is not avalable. Do you want to create? [Y/N]");
+					String dec = in.next();
+					if(dec.equalsIgnoreCase("Y")){
+						fileNameOk = true;
+						System.out.println("Enter file size in order of 2 (eg. 256):");
+						fs.fileSize = in.nextInt();
+						System.out.println("Enter file size in order of 2 (eg. 128):");
+						fs.blockSize = in.nextInt();
+						fs.create();
+					}
+				}
+				else
+				{
+					System.out.println("File system exists");
+					fileNameOk = true;
 				}
 			}
-		}
+		}while(!fileNameOk);
 		in.close();
 	}
 	
-	public void create(String fileName, int fileSize, int blockSize){
-		File file = new File(".."+sep+"Access"+sep+fileName);
+	public void create(){
+		File file = new File("src"+sep+"Assets"+sep+fileName);
 		try {
 			RandomAccessFile randAccFile = new RandomAccessFile(file, "rw");
 			randAccFile.setLength(fileSize);
@@ -49,20 +66,16 @@ public class FileSystem {
 			e.printStackTrace();
 		} 
 	}
-	public boolean isCreated(String fileName){
-		File file = new File(".."+sep+"Access"+sep+fileName);
+	public boolean isCreated(String fsName){
+		fileName = fsName;
+		File file = new File("src"+sep+"Assets"+sep+fileName);
 		return file.exists();
 	}
 	public void delete(String fileName){
 		
 	}
-	public void set(){
+	public void setFileSystem(){
 		
-	}
-	public static FileSystem get(String fileName){
-		FileSystem fs = new FileSystem();
-		
-		return fs;
 	}
 	
 }
